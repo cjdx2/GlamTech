d067a7df27814b30c93af25c079e39b4d9e6d4c9
kianndono <kiannredondo@gmail.com>
2024-06-12 20:33:14 +0800

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
         event.preventDefault();
         // Assuming you want to clear the session and redirect to the login page
         localStorage.clear();
-        window.location.href = "login.html"
+        window.location.href = "login.html";
     });
 
     // Event listener for menu button
